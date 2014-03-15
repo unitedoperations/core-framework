ccf6fdcb0fc2b6715701f5c1d1c365ea60891e64
Olsen <victor_olsen2@msn.com>
2014-03-15 02:19:25 +0100

    Add earplug dependency

--- modules/earplugs/module.cpp
+++ modules/earplugs/module.cpp
@@ -4,6 +4,7 @@ class earplugs
 	authors[] = {"Olsen"};
 	version = 1;
 	required_version = 1; 
+	dependencies[] = {"ace_sys_goggles"};
 	url = "https://github.com/unitedoperations/Core-framework";
 	postinit = "postinit.sqf";
 };
