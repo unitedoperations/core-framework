80917555088894cc9551cdb63c284e765c967aea
Dylan Plecki <dylanplecki@gmail.com>
2014-03-16 12:20:16 -0400

    Update gear module URL

--- modules/gear/module.cpp
+++ modules/gear/module.cpp
@@ -4,6 +4,7 @@ class gear
 	name = "Gear Module";
 	authors[] = {"Naught"};
 	version = 1;
+	url = "https://github.com/unitedoperations/Core-framework";
 	preinit = "preinit.sqf";
 	class settings {
 		#include "settings.cpp"
