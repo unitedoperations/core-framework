0731433f463b61a08cbd0f8e9fdd22ec1e68c4e0
Dylan Plecki <dylanplecki@gmail.com>
2014-03-17 14:10:40 -0400

    Add ai_spawner module

---
+++ modules/ai_spawner/settings.cpp
@@ -0,0 +1,12 @@
+
+class groups
+{
+	class ai_group_1
+	{
+		position = ""; // Code string
+		markers[] = {}; // Marker array
+		condition = ""; // Code string
+		init = ""; // Code string
+		units[] = {}; // Unit class array
+	};
+};
